a03668b3c45d2be35a8fb4a859e2488d2a6a4a02
U.N.Owen <425571681@qq.com>
2025-04-10 22:37:39 +0800

    实现音频管理子系统，可以通过该子系统播放2D或3D音频。同时为主界面添加了一个BGM。

--- Source/GameplayFramework/Private/Audio/AudioManagerSubsystem.cpp
+++ Source/GameplayFramework/Private/Audio/AudioManagerSubsystem.cpp
@@ -3,3 +3,68 @@
 
 #include "Audio/AudioManagerSubsystem.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Components/AudioComponent.h"
+
+void UAudioManagerSubsystem::PlaySound2D(FName InSoundID, float InStartTime, AActor* InOwningActor)
+{
+	UGameConfigSubsystem* Config = UGameInstance::GetSubsystem<UGameConfigSubsystem>(this->GetGameInstance());
+	FAudioManagerDataTableRow* TableRow = nullptr;
+	if (Config->GetDataTableRowFromID<FAudioManagerDataTableRow>(InSoundID, TableRow))
+	{
+		UGameplayStatics::PlaySound2D(this, TableRow->SoundWave, TableRow->VolumeMultiplier, TableRow->PitchMultiplier, InStartTime, TableRow->OverriddenConcurrencySettings, InOwningActor, TableRow->bIsUISound);
+	}
+}
+
+UAudioComponent* UAudioManagerSubsystem::SpawnSound2D(FName InSoundID, float InStartTime, bool bInPersistAcrossLevelTransition)
+{
+	UGameConfigSubsystem* Config = UGameInstance::GetSubsystem<UGameConfigSubsystem>(this->GetGameInstance());
+	FAudioManagerDataTableRow* TableRow = nullptr;
+	UAudioComponent* AudioComponent = nullptr;
+
+	if (Config->GetDataTableRowFromID<FAudioManagerDataTableRow>(InSoundID, TableRow))
+	{
+		AudioComponent = UGameplayStatics::SpawnSound2D(this, TableRow->SoundWave, TableRow->VolumeMultiplier, TableRow->PitchMultiplier, InStartTime, TableRow->OverriddenConcurrencySettings, bInPersistAcrossLevelTransition, TableRow->bAutoDestroy);
+	}
+
+	if (AudioComponent)
+	{
+		AudioComponent->AttenuationSettings = TableRow->OverriddenAttenuationSettings;
+		AudioComponent->bStopWhenOwnerDestroyed = TableRow->bStopWhenAttachedToDestroyed;
+		AudioComponent->bIsUISound = TableRow->bIsUISound;
+	}
+
+	return AudioComponent;
+}
+
+void UAudioManagerSubsystem::PlaySound3D(FName InSoundID, FVector InLocation, FRotator InRotation, float InStartTime, AActor* InOwningActor)
+{
+	UGameConfigSubsystem* Config = UGameInstance::GetSubsystem<UGameConfigSubsystem>(this->GetGameInstance());
+	FAudioManagerDataTableRow* TableRow = nullptr;
+	if (Config->GetDataTableRowFromID<FAudioManagerDataTableRow>(InSoundID, TableRow))
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, TableRow->SoundWave, InLocation, InRotation, TableRow->VolumeMultiplier, TableRow->PitchMultiplier, InStartTime, TableRow->OverriddenAttenuationSettings, TableRow->OverriddenConcurrencySettings, InOwningActor);
+	}
+}
+
+UAudioComponent* UAudioManagerSubsystem::SpawnSound3D(FName InSoundID, FVector InLocation, FRotator InRotation, float InStartTime, bool bInPersistAcrossLevelTransition)
+{
+	UGameConfigSubsystem* Config = UGameInstance::GetSubsystem<UGameConfigSubsystem>(this->GetGameInstance());
+	FAudioManagerDataTableRow* TableRow = nullptr;
+	UAudioComponent* AudioComponent = nullptr;
+
+	if (Config->GetDataTableRowFromID<FAudioManagerDataTableRow>(InSoundID, TableRow))
+	{
+		AudioComponent = UGameplayStatics::SpawnSoundAtLocation(this, TableRow->SoundWave, InLocation, InRotation, TableRow->VolumeMultiplier, TableRow->PitchMultiplier, InStartTime, TableRow->OverriddenAttenuationSettings, TableRow->OverriddenConcurrencySettings, TableRow->bAutoDestroy);
+	}
+
+	if (AudioComponent)
+	{
+		AudioComponent->AttenuationSettings = TableRow->OverriddenAttenuationSettings;
+		AudioComponent->bStopWhenOwnerDestroyed = TableRow->bStopWhenAttachedToDestroyed;
+		AudioComponent->bIsUISound = TableRow->bIsUISound;
+	}
+
+	return AudioComponent;
+}
+
--- Source/GameplayFramework/Public/Audio/AudioManagerSubsystem.h
+++ Source/GameplayFramework/Public/Audio/AudioManagerSubsystem.h
@@ -13,8 +13,34 @@ struct FAudioManagerDataTableRow : public FAYRTableRowBase
 {
 	GENERATED_BODY()
 
+	// 要播放的音频资产。
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
 	USoundWave* SoundWave = nullptr;
+
+	// 音量系数。
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	float VolumeMultiplier = 1.f;
+
+	// 音调系数。
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	float PitchMultiplier = 1.f;
+
+	// 并发设置。用于配置当该音频同时播放多次时的表现。会覆盖Sound Wave或Sound Cue里的并发设置。
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	USoundConcurrency* OverriddenConcurrencySettings = nullptr;
+
+	// 音频衰减设置。会覆盖Sound Wave或Sound Cue里的音频衰减设置。
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	USoundAttenuation* OverriddenAttenuationSettings = nullptr;
+
+	// 是否为UI音频。如果为UI音频的话当游戏暂停后依然会播放。
+	bool bIsUISound = false;
+
+	// 是否播放完后销毁。对“PlaySound2D”和“PlayerSound3D”不起效。
+	bool bAutoDestroy = true;
+
+	// 是否在附加对象被销毁后停止播放。对“PlaySound2D”和“PlayerSound3D”不起效。
+	bool bStopWhenAttachedToDestroyed = false;
 };
 
 /**
@@ -25,4 +51,19 @@ class GAMEPLAYFRAMEWORK_API UAudioManagerSubsystem : public UGameInstanceSubsyst
 {
 	GENERATED_BODY()
 	
+	// 生成并播放2D音频。适用于播放UI音效、BGM等。
+	UFUNCTION(BlueprintCallable, Category = "Audio|2D")
+	void PlaySound2D(FName SoundID, float StartTime = -1.f, AActor* OwningActor = nullptr);
+
+	// 生成并播放2D音频，同时返回Audio Component供后续使用。
+	UFUNCTION(BlueprintCallable, Category = "Audio|2D")
+	UAudioComponent* SpawnSound2D(FName SoundID, float StartTime = -1.f, bool bPersistAcrossLevelTransition = false);
+
+	// 生成并播放3D音频，在指定Location和Rotation上生成空间音效。
+	UFUNCTION(BlueprintCallable, Category = "Audio|3D")
+	void PlaySound3D(FName SoundID, FVector Location, FRotator Rotation, float StartTime = -1.f, AActor* OwningActor = nullptr);
+
+	// 生成并播放3D音频，同时返回Audio Component供后续使用。
+	UFUNCTION(BlueprintCallable, Category = "Audio|3D")
+	UAudioComponent* SpawnSound3D(FName SoundID, FVector Location, FRotator Rotation, float StartTime = -1.f, bool bPersistAcrossLevelTransition = false);
 };
