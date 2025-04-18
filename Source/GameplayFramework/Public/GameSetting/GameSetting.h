8fdcf9939563ccb00eae15e0b9ef43ddedfe3ea1
U.N.Owen <425571681@qq.com>
2025-04-18 23:53:50 +0800

    例行提交。创建游戏设置空子系统，以后音量、分辨率等设置都在这个子系统里处理。

---
+++ Source/GameplayFramework/Public/GameSetting/GameSetting.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GameSetting.generated.h"
+
+/**
+ * 游戏设置相关子系统。音量、分辨率等相关设置。
+ */
+UCLASS()
+class GAMEPLAYFRAMEWORK_API UGameSetting : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
