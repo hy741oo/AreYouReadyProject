9801947d10f578144ca71e354ca3c9c253b6de9e
U.N.Owen <425571681@qq.com>
2025-04-19 10:32:38 +0800

    更改游戏设置子系统的类名和文件名。

--- Source/GameplayFramework/Public/GameSetting/GameSettingSubsystem.h
+++ Source/GameplayFramework/Public/GameSetting/GameSettingSubsystem.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
-#include "GameSetting.generated.h"
+#include "GameSettingSubsystem.generated.h"
 
 /**
  * 游戏设置相关子系统。音量、分辨率等相关设置。
  */
 UCLASS()
-class GAMEPLAYFRAMEWORK_API UGameSetting : public UGameInstanceSubsystem
+class GAMEPLAYFRAMEWORK_API UGameSettingSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
 	
