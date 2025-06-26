ea36a53c9dca48540bc9a59c5ca92a6a741983c4
U.N.Owen <425571681@qq.com>
2025-06-26 22:04:25 +0800

    为MainLevelCharacter添加视角移动逻辑。

--- Source/GameplayFramework/Private/MainLevel/MainLevelCharacter.cpp
+++ Source/GameplayFramework/Private/MainLevel/MainLevelCharacter.cpp
@@ -5,6 +5,26 @@
 
 #include "Input/PlayerInputSubsystem.h"
 #include "MainLevel/MainLevelPlayerController.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+
+AMainLevelCharacter::AMainLevelCharacter(const FObjectInitializer& InObjectInitializer)
+{
+	// 生成摄像机。
+	this->PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Character Camera"));
+	if (this->PlayerCamera)
+	{
+		this->PlayerCamera->SetupAttachment(this->GetRootComponent());
+		this->PlayerCamera->SetRelativeLocation(this->GetPawnViewLocation());
+	}
+
+	// 设置胶囊体尺寸。
+	if (UCapsuleComponent* PlayerCapsule = this->GetCapsuleComponent())
+	{
+		PlayerCapsule->SetCapsuleRadius(42);
+		PlayerCapsule->SetCapsuleHalfHeight(96);
+	}
+}
 
 void AMainLevelCharacter::BeginPlay()
 {
@@ -15,11 +35,17 @@ void AMainLevelCharacter::BeginPlay()
 	{
 		if (UPlayerInputSubsystem* PlayerInputSubsystem = ULocalPlayer::GetSubsystem<UPlayerInputSubsystem>(OwningController->GetLocalPlayer()))
 		{
-			// 前后运动。
+			// 前后移动。
 			PlayerInputSubsystem->BindPlayerInputAction("MainLevel_MoveForward", this, &AMainLevelCharacter::MoveForward);
 
-			// 左右运动。
+			// 左右移动。
 			PlayerInputSubsystem->BindPlayerInputAction("MainLevel_MoveRight", this, &AMainLevelCharacter::MoveRight);
+
+			// 左右移动镜头。
+			PlayerInputSubsystem->BindPlayerInputAction("MainLevel_LookAround", this, &AMainLevelCharacter::LookAround);
+
+			// 上下移动镜头。
+			PlayerInputSubsystem->BindPlayerInputAction("MainLevel_LookUp", this, &AMainLevelCharacter::LookUp);
 
 			// 基础运动的IMC。
 			PlayerInputSubsystem->AddPlayerInputMappingContext("MainLevel_Movement");
@@ -42,6 +68,29 @@ void AMainLevelCharacter::MoveRight(const FInputActionInstance& InValue)
 	if (UPlayerInputSubsystem::GetAxis1DTypeInstanceValue(InValue, Value))
 	{
 		this->AddMovementInput(this->GetActorRightVector(), Value);
+	}
+}
+void AMainLevelCharacter::LookAround(const FInputActionInstance& InValue)
+{
+	float Value = .0f;
+	if (UPlayerInputSubsystem::GetAxis1DTypeInstanceValue(InValue, Value))
+	{
+		this->AddControllerYawInput(Value);
+	}
+}
+
+void AMainLevelCharacter::LookUp(const FInputActionInstance& InValue)
+{
+	float Value = .0f;
+	if (UPlayerInputSubsystem::GetAxis1DTypeInstanceValue(InValue, Value))
+	{
+		if (this->PlayerCamera)
+		{
+			FRotator LastRotator = this->PlayerCamera->GetRelativeRotation();
+			Value = FMath::Clamp(LastRotator.Pitch + Value, -70.f, 70.f);
+			FRotator NewRotator(Value, .0f, .0f);
+			this->PlayerCamera->SetRelativeRotation(NewRotator);
+		}
 	}
 }
 
--- Source/GameplayFramework/Public/MainLevel/MainLevelCharacter.h
+++ Source/GameplayFramework/Public/MainLevel/MainLevelCharacter.h
@@ -4,9 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "AYRCharacter.h"
+
 #include "MainLevelCharacter.generated.h"
 
 struct FInputActionInstance;
+class UCameraComponent;
 
 /**
  * 
@@ -16,8 +18,14 @@ class GAMEPLAYFRAMEWORK_API AMainLevelCharacter : public AAYRCharacter
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	UCameraComponent* PlayerCamera = nullptr;
 
 public:
+	// 构造函数
+	AMainLevelCharacter(const FObjectInitializer& InObjectInitializer = FObjectInitializer::Get());
+
 	// 执行绑定运动等操作。
 	virtual void BeginPlay() override;
 
@@ -26,4 +34,10 @@ public:
 
 	// 左右移动逻辑。
 	virtual void MoveRight(const FInputActionInstance& InValue);
+
+	// 左右移动摄像机逻辑。
+	virtual void LookAround(const FInputActionInstance& InValue);
+
+	// 上下移动摄像机逻辑。
+	virtual void LookUp(const FInputActionInstance& InValue);
 };
