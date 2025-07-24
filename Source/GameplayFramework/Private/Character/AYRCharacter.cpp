316a5ed151ae188cad00b4cd20d05e8086f0e04c
U.N.Owen <425571681@qq.com>
2025-07-24 18:24:29 +0800

    将角色基类移动到Character文件夹里。

--- Source/GameplayFramework/Private/Character/AYRCharacter.cpp
+++ Source/GameplayFramework/Private/Character/AYRCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AYRCharacter.h"
+#include "Character/AYRCharacter.h"
 
 // Sets default values
 AAYRCharacter::AAYRCharacter()
